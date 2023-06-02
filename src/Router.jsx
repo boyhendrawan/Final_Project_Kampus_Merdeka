b937adfaf3f44f88e269ab7712839f1fba5ab741
BOY HENDRAWAN PURBA <boyhendrawanpurba@example.com>
2023-06-02 20:59:29 +0700

    fix routers

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {createBrowserRouter} from "react-router-dom";
 import Dashboard from './pages/Customers/Dashboard';
 import Index from './pages/Customers/Index';
+import AboutUs from './pages/Customers/AboutUs';
 // this file contain all the Route of this apps
 const Router=createBrowserRouter([
     {
@@ -11,6 +12,10 @@ const Router=createBrowserRouter([
             {
                 index:true,
                 element:<Dashboard/>,
+            },
+            {
+                path:"aboutUs",
+                element:<AboutUs/>,
             }
         ]
         
