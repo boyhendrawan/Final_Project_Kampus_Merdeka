b78422b1798f1e82d4f86f8d7e66c84cf6f3cc5e
BOY HENDRAWAN PURBA <boyhendrawanpurba@example.com>
2023-07-06 19:12:36 +0700

    fixed navbar

--- src/pages/Customers/Notifications.jsx
+++ src/pages/Customers/Notifications.jsx
@@ -1,10 +1,7 @@
-import { IoMdArrowBack, IoMdNotificationsOutline } from "react-icons/io";
-import { MdEventNote, MdOutlineAccountCircle } from "react-icons/md";
-import { BiHomeAlt } from "react-icons/bi";
 import CardNotification from "../../components/CardNotification";
+import { IoMdArrowBack } from "react-icons/io";
 import { Link } from "react-router-dom";
 import React from "react";
-import logo from "../../assets/logo.png";
 
 const Notifications = () => {
   const promoData = [
@@ -52,38 +49,6 @@ const Notifications = () => {
           ))}
         </div>
         <span className="text-slate-500 font-thin text-xs">version 1.1.0</span>
-      </div>
-      <div className="fixed bottom-0 left-0 w-full shadow-md z-20 border-y-[1px] border-primary-darkblue04 py-2 px-4 md:bottom-auto md:top-0 md:mt-2">
-        <div className="md:flex md:justify-between md:ml-24 lg:32 xl:ml-44 overflow-x-hidden">
-          <div className="hidden md:flex">
-            <img src={logo} alt="Logo" className="h-[50px]" />
-          </div>
-          <div className="flex items-center justify-between md:mr-24 lg:32 xl:mr-44">
-            <div className="mr-4 flex flex-col items-center text-slate-500 md:hidden">
-              <BiHomeAlt size={25} />
-              <span className="text-xs mt-1">Beranda</span>
-            </div>
-            <div className="mr-4 flex flex-col items-center text-slate-500">
-              <MdEventNote size={25} />
-              <span className="text-xs mt-1 md:hidden">Riwayat</span>
-            </div>
-            <div className="mr-4 flex flex-col items-center text-primary-darkblue04">
-              <Link to={"/user/notification"}>
-                <IoMdNotificationsOutline size={25} />
-                <span className="text-xs mt-1 md:hidden">Notifikasi</span>
-              </Link>
-            </div>
-            <div className="mr-4 flex flex-col items-center text-slate-500 hover:text-primary-darkblue04">
-              <Link to={"/user/account"}>
-                <MdOutlineAccountCircle
-                  className="flex items-center"
-                  size={25}
-                />
-                <span className="font-bold text-xs mt-1 md:hidden">Akun</span>
-              </Link>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
